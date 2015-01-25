378a497b46bdb053abef7d99087142d413497149
vhb <vctrhb@gmail.com>
2015-01-25 12:17:46 -0500

    add cargo install script

---
+++ install_cargo.sh
@@ -0,0 +1,5 @@
+triple=x86_64-apple-darwin
+curl -O https://static.rust-lang.org/cargo-dist/cargo-nightly-$triple.tar.gz
+tar xf cargo-nightly-$triple.tar.gz
+sudo ./cargo-nightly-$triple/install.sh
+rm -fr cargo-nightly-$triple.tar.gz cargo-nightly-$triple

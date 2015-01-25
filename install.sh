d00b64f4d832cec33d35e3235ee7f6e5498413e4
vhb <vctrhb@gmail.com>
2015-01-25 18:14:19 +0100

    fix spf13 install

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 wget --no-check-certificate http://install.ohmyz.sh -O - | sh
-curl https://j.mp/spf13-vim3 -L -o - | sh
-git clone https://github/vhb/dotfiles.git
+wget  --no-check-certificate https://raw.githubusercontent.com/spf13/spf13-vim/3.0/bootstrap.sh -O - | sh
+# git clone https://github/vhb/dotfiles.git

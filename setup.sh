7a6ae21947b163d5aa6444b3c3d7dd4e340dd1bb
vhb <vctrhb@gmail.com>
2015-05-04 11:42:47 -0400

    some fixes and add a few vim plugins

--- setup.sh
+++ setup.sh
@@ -7,5 +7,12 @@ ln -sf $current_path/./irssi ~/.irssi
 ln -sf $current_path/./vim/vimrc.before.local ~/.vimrc.before.local
 ln -sf $current_path/./vim/vimrc.local ~/.vimrc.local
 ln -sf $current_path/./vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
+
+# Vimproc need to be installed manualy
+git clone https://github.com/Shougo/vimproc.vim.git vimproc.vim
+cd vimproc.vim && make
+ln -sf $current_path/vimproc.vim ~/.vim/bundle/vimproc.vim
+
 vim +BundleInstall
+
 cd ~/.vim/bundle/YouCompleteMe && sh install.sh --clang-completer
